3765cbdae0eaee7e2e59fc747ee7e207c0ff87ff
Akshukla12 <114152828+Akshukla12@users.noreply.github.com>
2025-07-06 13:55:37 +0530

    Update middleware.js

--- middleware.js
+++ middleware.js
@@ -3,17 +3,6 @@
 import { clerkMiddleware, createRouteMatcher } from "@clerk/nextjs/server";
 import { NextResponse } from "next/server";
 
-// 🛑 HARDCODED KEYS (not recommended in production)
-const clerkConfig = {
-  secretKey: "sk_test_hCXaN0HYWdMBQZQ1iY7XF6xHPkg70eeaCvWD9XSeOV",
-  publishableKey: "pk_test_YWN0aXZlLWZsYW1pbmdvLTE3LmNsZXJrLmFjY291bnRzLmRldiQ"
-  
-};
-
-const middleware = clerkMiddleware({
-  ...clerkConfig
-});
-
 const isProtectedRoute = createRouteMatcher([
   "/dashboard(.*)",
   "/resume(.*)",
@@ -22,20 +11,20 @@ const isProtectedRoute = createRouteMatcher([
   "/onboarding(.*)"
 ]);
 
-export default middleware(async (auth, req) => {
-  const { userId } = await auth();
+export default clerkMiddleware((auth, req) => {
+  const { userId } = auth();
 
   if (userId || !isProtectedRoute(req)) {
     return NextResponse.next();
   }
 
-  const { redirectToSignIn } = await auth();
+  const { redirectToSignIn } = auth();
   return redirectToSignIn();
 });
 
 export const config = {
   matcher: [
-    "/((?!.*\\..*|_next).*)",  // ignore _next/** and public files
-    "/(api|trpc)(.*)"          // match APIs too
+    "/((?!.*\\..*|_next).*)", // Exclude static files
+    "/(api|trpc)(.*)"
   ]
 };
